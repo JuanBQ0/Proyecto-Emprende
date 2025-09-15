d60213c125d157394ceea9132c1beee01c7b5e59
Lord Snow <117885464+JuanBQ0@users.noreply.github.com>
2025-09-15 14:15:17 -0500

    Actualización de componentes

--- src/app/proyectos/pages/administrativo/gestion-proyectos/gestion-proyectos.component.ts
+++ src/app/proyectos/pages/administrativo/gestion-proyectos/gestion-proyectos.component.ts
@@ -2,18 +2,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NavAdministrativoComponent } from "../../../components/componente administrativo/nav-administrativo/nav-administrativo.component";
 import { ProyectoServices } from '../../../services/proyectos.services';
 import { ProyectoRegistrado } from '../../../interface/proyecto.interface';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
+import { DrawGaleriaComponent } from "../../../components/Draw_Galeria/Draw_Galeria.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-proyectos',
   standalone: true,
-  imports: [NavAdministrativoComponent, DatePipe],
+  imports: [NavAdministrativoComponent, DatePipe, DrawGaleriaComponent, CommonModule, FormsModule],
   templateUrl: './gestion-proyectos.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GestionProyectosComponent implements OnInit {
 
   proyectos: ProyectoRegistrado[] = [];
+mostrarFiltros: any;
 
   constructor(private proyectoService: ProyectoServices) {}
 
